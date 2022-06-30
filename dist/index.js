643aa71c8aa67343c86f636082f681beb7ecbcb8
Zheng <defrag55345@gmail.com>
2022-06-30 22:26:19 +0800

    add config folder & many package

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
-console.log("hellds57554ddodd");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+const config_1 = __importDefault(require("config"));
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = config_1.default.get("port");
+app.use(express.json());
+app.use(cors());
+app.listen(port, () => console.log(`Server is up on the ${port}`));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hellds57554ddodd");
+// require("./db/mongoose");
+require("dotenv").config();
+import config from "config";
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = config.get("port");
+
+app.use(express.json());
+app.use(cors());
+
+// const userRouter = require("../src/routers/user");
+// const couponRouter = require("../src/routers/coupon");
+// const productRouter = require("../src/routers/product");
+// const orderRouter = require("../src/routers/order");
+// const gameRouter = require("../src/routers/game");
+
+// app.use("/api", userRouter);
+// app.use("/api", couponRouter);
+// app.use("/api", productRouter);
+// app.use("/api", orderRouter);
+// app.use("/api", gameRouter);
+
+app.listen(port, () => console.log(`Server is up on the ${port}`));
