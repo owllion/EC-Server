46620fb756829fbc4bacdfa8bcefd657b488ebab
Zheng <defrag55345@gmail.com>
2022-07-09 15:30:26 +0800

    coupon.model finish

---
+++ src/model/coupon.model.ts
@@ -0,0 +1,27 @@
+import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
+
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class Coupon {
+  @prop({ required: true })
+  code: string;
+
+  @prop()
+  description: string;
+
+  @prop({ required: true })
+  discount_type: string;
+
+  @prop({ required: true })
+  amount: number;
+
+  @prop({ required: true })
+  expiry_date: Date;
+}
+
+const CouponModel = getModelForClass(Coupon);
+
+export default CouponModel;
