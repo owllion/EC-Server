946ae21c067bef946905844aff0af4c33e0266ea
owllion <defrag55345@gmail.com>
2022-09-11 16:39:01 +0800

    reinstall

--- src/@types/general.d.ts
+++ src/@types/general.d.ts
@@ -1,3 +1,4 @@
 declare module "shortid";
 declare module "google-auth-library";
+declare module "helmet";
 //If a module does not have types,then declare module 'packageName',the error will disappear.
